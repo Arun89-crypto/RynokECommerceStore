2c0a8549e15e2a9a34127eebf80b1aac893560e4
Arun Jangra <arunjngra89@gmail.com>
2021-12-22 19:19:30 +0530

    fix: folder struct 📁

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ const port = 8000;
 app.use(express.json());
 
 // Defining the routes for all the operations
-app.use('/api/auth', require('./routes/auth'));
+app.use('/api/auth', require('./routes/User/auth'));
 // app.use('/api/product', require('./routes/product'));
 // app.use('/api/review', require('./routes/review'));
 // app.use('/api/cart', require('./routes/cart'));
--- routes/User/auth.js
+++ routes/User/auth.js
@@ -1,10 +1,10 @@
 const express = require('express');
 const router = express.Router();
-const User = require('../models/User');
+const User = require('../../models/User');
 const { body, validationResult } = require('express-validator');
 const bcrypt = require('bcryptjs');
 const jwt = require('jsonwebtoken');
-const fetchUser = require('../middleware/fetchuser');
+const fetchUser = require('../../middleware/fetchuser');
 
 const JWT_SECRET = "teetarkeaageteetar"
 
