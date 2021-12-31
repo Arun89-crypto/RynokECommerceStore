bfd43b404a04450dfafd2e5b3c3e2e6148e103b5
Arun Jangra <arunjngra89@gmail.com>
2021-12-31 15:53:13 +0530

    tem : frontend main template ready

---
+++ client/src/Components/LandingPage/LandingPage.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import Navbar from '../Navbar/Navbar'
+
+const LandingPage = () => {
+    return (
+        <div>
+            <Navbar />
+            <h1>Rynok E-Commerce.</h1>
+        </div>
+    )
+}
+
+export default LandingPage
